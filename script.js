44de87a9373c46e1bc8a3facdcf9dc477b4ddeb7
Achinta <ecachinta4@gmail.com>
2021-06-02 06:52:42 +0000

    changes made from Atom

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var y = document.getElementById("years").value;
+    var interest = p * y * r /100;
+    var year = new Date().getFullYear()+parseInt(y);
+    var amount = p + interest;
+    document.getElementById("result").innerHTML="If you deposit "+p+",\<br\>at an interest rate of "+r+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    document.catch(error => console.error('Error!', error.message))
 }
-        
+function updateRate()
+{
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText=rateval;
+}
